5c3eeed1dc72691207c92fb39681ad09f6cbfc72
VinMoria <cyo01@outlook.com>
2024-11-14 00:29:07 +0800

    add image

--- main_chatgpt.py
+++ main_chatgpt.py
@@ -13,26 +13,27 @@ with open("config.json") as f:
 _log = logging.get_logger()
 history_context = []
 
-def receive_and_gen(in_str):
-	in_str = in_str.strip()
-	print(in_str)
-	# 指令模式
-	if in_str[0] == "~":
-		#  修改chatgpt人设
-		if in_str[0:2] == "~s":
-			role_text = in_str[3:]
-			chatgpt_system_set(role_text)
-			res = f"修改chatgpt人设: [{role_text}]"
-		# 清楚上下文
-		elif in_str[0:2] == "~c":
-			chatgpt_clean_history()
-			res = "清除 chatgpt 上下文记忆"
-		else:
-			res = "指令不存在"
-	# 对话模式
-	else:
-		res = chatgpt_talk(in_str)
-	return res
+# def receive_and_gen(in_str):
+# 	in_str = in_str.strip()
+# 	# 指令模式
+# 	if in_str[0] == "~":
+# 		#  修改chatgpt人设
+# 		if in_str[0:2] == "~s":
+# 			role_text = in_str[3:]
+# 			chatgpt_system_set(role_text)
+# 			res = f"修改chatgpt人设: [{role_text}]"
+# 		# 清楚上下文
+# 		elif in_str[0:2] == "~c":
+# 			chatgpt_clean_history()
+# 			res = "清除 chatgpt 上下文记忆"
+# 		elif in_str[0:2] == "~i":
+# 			image_prompt = in_str[3:]
+# 		else:
+# 			res = "指令不存在"
+# 	# 对话模式
+# 	else:
+# 		res = chatgpt_talk(in_str)
+# 	return res
 
 def chatgpt_system_set(role_text):
 	history_context.append({"role": "system", "content": role_text})
@@ -63,17 +64,69 @@ def chatgpt_gen():
 	# 返回助手的回复
 	return assistant_reply
 
+def chatgpt_gen_image(in_prompt):
+	response = gpt_client.images.generate(
+	model="dall-e-3",
+	prompt=in_prompt,
+	size="1024x1024",
+	quality="standard",
+	n=1,
+	)
+	image_url = response.data[0].url
+	print(image_url)
+	return image_url
+
 class MyClient(botpy.Client):
 	async def on_ready(self):
 		_log.info(f"robot 「{self.robot.name}」 on_ready!")
 
 	async def on_group_at_message_create(self, message: GroupMessage):
+		in_str = message.content.strip()
+		# 指令模式
+		if in_str[0] == "~":
+			#  修改chatgpt人设
+			if in_str[0:2] == "~s":
+				role_text = in_str[3:]
+				chatgpt_system_set(role_text)
+				await self.send_text(message, f"修改chatgpt人设: [{role_text}]")
+			# 清楚上下文
+			elif in_str[0:2] == "~c":
+				chatgpt_clean_history()
+				await self.send_text(message, "清除 chatgpt 上下文记忆")
+			# 图片生成
+			elif in_str[0:2] == "~i":
+				image_prompt = in_str[3:]
+				await self.send_image(message, image_prompt)
+			else:
+				await self.send_text(message, "指令不存在")
+		# 对话模式
+		else:
+			await self.send_text(message, chatgpt_talk(in_str))
+
+	async def send_text(self, message, send_content):
 		messageResult = await message._api.post_group_message(
 			group_openid=message.group_openid,
-			  msg_type=0, 
-			  msg_id=message.id,
-			  content=receive_and_gen(message.content))
+			msg_type=0, 
+			msg_id=message.id,
+			content=send_content)
 		_log.info(messageResult)
+
+	async def send_image(self, message, image_prompt):
+		image_url = chatgpt_gen_image(image_prompt)
+
+		uploadMedia = await message._api.post_group_file(
+			group_openid=message.group_openid,
+			file_type=1,
+			url=image_url
+		)
+
+		await message._api.post_group_message(
+			group_openid=message.group_openid,
+			msg_type=7,
+			msg_id=message.id,
+			msg_seq=1,
+			media=uploadMedia
+		)
 
 if __name__ == "__main__":
 	# Chatgpt
